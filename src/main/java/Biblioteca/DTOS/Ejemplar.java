26637aa3045fd40d1d48507060553df0649087d9
AliciaPM08 <apachecomena@gmail.com>
2024-12-09 18:13:06 +0100

    Clase Ejemplar terminada

--- src/main/java/Biblioteca/DTOS/Ejemplar.java
+++ src/main/java/Biblioteca/DTOS/Ejemplar.java
@@ -1,4 +1,4 @@
-package Biblioteca;
+package Biblioteca.DTOS;
 
 import jakarta.persistence.*;
 import org.hibernate.annotations.ColumnDefault;
@@ -11,24 +11,28 @@ import java.util.Set;
 @Entity
 @Table(name = "ejemplar")
 public class Ejemplar {
+    //Variables
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Integer id;
 
+    //He cambiado isbn por libro, por si necesito algo mas de esa clase
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "isbn", nullable = false)
-    private Biblioteca.Libro isbn;
+    private Libro libro;
 
     @ColumnDefault("'Disponible'")
     @Lob
     @Column(name = "estado")
     private String estado;
 
+    //Vinculacion con la clase prestamos
     @OneToMany(mappedBy = "ejemplar")
-    private Set<Biblioteca.Prestamo> prestamos = new LinkedHashSet<>();
+    private Set<Prestamo> prestamos = new LinkedHashSet<>();
 
+    //Getters y setters
     public Integer getId() {
         return id;
     }
@@ -37,12 +41,12 @@ public class Ejemplar {
         this.id = id;
     }
 
-    public Biblioteca.Libro getIsbn() {
-        return isbn;
+    public Libro getLibro() {
+        return libro;
     }
 
-    public void setIsbn(Biblioteca.Libro isbn) {
-        this.isbn = isbn;
+    public void setLibro(Libro libro) {
+        this.libro = libro;
     }
 
     public String getEstado() {
@@ -53,12 +57,13 @@ public class Ejemplar {
         this.estado = estado;
     }
 
-    public Set<Biblioteca.Prestamo> getPrestamos() {
+    public Set<Prestamo> getPrestamos() {
         return prestamos;
     }
 
-    public void setPrestamos(Set<Biblioteca.Prestamo> prestamos) {
+    public void setPrestamos(Set<Prestamo> prestamos) {
         this.prestamos = prestamos;
     }
+
 
 }
