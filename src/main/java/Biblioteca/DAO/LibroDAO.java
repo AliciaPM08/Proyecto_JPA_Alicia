95352ae8b73fe777f99281535b98f3aa9fab8963
AliciaPM08 <apachecomena@gmail.com>
2024-12-09 18:35:35 +0100

    Clase LibroDAO terminada

---
+++ src/main/java/Biblioteca/DAO/LibroDAO.java
@@ -0,0 +1,24 @@
+package Biblioteca.DAO;
+
+import Biblioteca.DTOS.Libro;
+import jakarta.persistence.EntityManager;
+
+public class LibroDAO {
+    private EntityManager em;
+
+    public LibroDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    //Insertar Libros
+    public void insert(Libro libro) {
+        em.getTransaction().begin();
+        em.persist(libro);
+        em.getTransaction().commit();
+    }
+
+    //Encontrar Libros mediante el ISBN
+    public Libro findById(int isbn) {
+        return em.find(Libro.class, isbn);
+    }
+}
